b1d3992b53a7b8f15432470a5918126c3d73970a
david nguyen <davidngy2803@gmail.com>
2026-02-07 01:29:18 +0100

    adding parser

--- README.md
+++
@@ -1,2 +0,0 @@
-# TerminalPortfolio
-Terminal based Portfolio :)
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("terminal starting...");
+const input = document.querySelector<HTMLInputElement>("#input");
+if (!input) throw new Error("Missing input");
+const output = document.getElementById("output");
+if (!output) throw new Error("Missing output");
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    output.innerText = input.value;
+  }
+});
